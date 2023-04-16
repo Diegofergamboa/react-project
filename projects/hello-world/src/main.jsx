6b6497f333a5a3eeed02e261ddaa9573f78e142c
Diegofergamboa <diegofergamboa@hotmail.es>
2023-04-15 19:17:46 -0500

    Deploy with Vite

--- projects/hello-world/src/main.jsx
+++ projects/hello-world/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-) 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const CreateButton = ({text}) => {
+  return (
+    <button>{text}</button>
+  )
+}
+
+root.render(
+  <React.Fragment>
+    <CreateButton text='Primer button'/>
+  </React.Fragment>
+)
